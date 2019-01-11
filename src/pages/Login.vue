41721b2d934a12d8e0894aa6fa385d6b80fc0640
realeve@qq.com <realeve@qq.com>
2019-01-11 22:23:10 +0800

    update date

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -109,7 +109,7 @@ export default {
     isEnd() {
       return (
         dateFormat(new Date(), "YYYY-MM-DD HH:mm:ss") >
-        (this.is_teacher ? "2019-01-14 09:00:00" : "2019-01-11 16:00:00")
+        (this.is_teacher ? "2019-01-14 17:00:00" : "2019-01-11 16:00:00")
       );
     },
     is_teacher() {
